01415e7e0c3d9815043d7c37f7b3cb490e175472
wistcc <wistcc@hotmail.com>
2016-12-15 16:27:00 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,73 @@
+var cards = require('./cards');
+var availableCards = {};
+var deck = {};
+var currentWeight = 100;
+
+exports.getAllCards = function () {
+    return cards.all;
+}
+
+exports.getBalancedGame = function (players) {
+    while (currentWeight < -1 || currentWeight > 1) {
+        setGame(players);
+    }
+
+    return {
+        deck: deck,
+        weight: currentWeight
+    };
+}
+
+function setGame(players) {
+    resetValues();
+    //get first card randomly
+    addCardToDeck(getRandom(0, 1));
+    players--;
+
+    for (var i = 0; i < players; i++) {
+        addCardToDeck(currentWeight >= 0);
+    }
+}
+
+function resetValues() {
+    deck = {};
+    currentWeight = 0;
+    availableCards = JSON.parse(JSON.stringify(cards));
+}
+
+function addCardToDeck(isNegative) {
+    while (true) {
+        if (isNegative) {
+            var rand = getRandom(0, availableCards.negatives.length - 1);
+            if (availableCards.negatives[rand].amount > 0) {
+                addRandomCard(availableCards.negatives[rand]);
+                break;
+            }
+        }
+        else {
+            var rand = getRandom(0, availableCards.nonnegatives.length - 1);
+            if (availableCards.nonnegatives[rand].amount > 0) {
+                addRandomCard(availableCards.nonnegatives[rand]);
+                break;
+            }
+        }
+    }
+}
+
+function addRandomCard(selectedCard) {
+    currentWeight += selectedCard.value;
+    selectedCard.amount--;
+
+    if (deck[selectedCard.role]) {
+        deck[selectedCard.role]++;
+    }
+    else {
+        deck[selectedCard.role] = 1;
+    }
+}
+
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
