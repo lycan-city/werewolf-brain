1e383f1654f3ae0770fa2e951de0a72dd77a8efb
wistcc <wistcc@hotmail.com>
2016-12-19 14:15:57 -0400

    Fixing template structure

--- templates.js
+++ templates.js
@@ -1,15 +1,59 @@
-exports.templates = {
-    'basic': 1,
-    'novice': 2,
-    'amateur': 4,
-    'wolfpack': 8,
-    'competent': 16
-};
-
-exports.none = 0;
-exports.all = 31;
-exports.basic = 1;
-exports.novice = 2;
-exports.amateur = 4;
-exports.wolfpack = 8;
-exports.competent = 16;
+exports.all = {
+    basic: [
+        'Werewolf',
+        'Villager',
+    ],
+    novice: [
+        'Werewolf',
+        'Villager',
+        'Seer',
+        'Mayor'
+    ],
+    amateur: [
+        'Wereeolf',
+        'Wolf cub',
+        'Villager',
+        'Mayor',
+        'Seer',
+        'Witch',
+        'Prince',
+        'Tanner'
+    ],
+    wolfpack: [
+        'Wereeolf',
+        'Big Bad Wolf',
+        'Wolf Cub',
+        'Lone Wolf',
+        'Fruit Brute',
+        'Dream Wolf',
+        'Villager',
+        'Seer',
+        'Prince',
+        'Mayor',
+        'Martyr'
+    ],
+    competent: [
+        'Wereeolf',
+        'Wolf cub',
+        'Sorcerer',
+        'Cursed',
+        'Minion',
+        'Wild Child',
+        'Villager',
+        'Mayor',
+        'Seer',
+        'Witch',
+        'Prince',
+        'Cult Leader',
+        'Cupid',
+        'Diseased',
+        'Doppelgänger',
+        'Lycan',
+        'Spellcaster',
+        'Tanner',
+        'Mason',
+        'Bodyguard',
+        'Apprentice Seer',
+        'Drunk'
+    ]
+}
