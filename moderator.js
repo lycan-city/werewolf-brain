7659efae0cccde74e97f51109a2ffb385754311d
Tomás Veras <tomasverasm@gmail.com>
2017-01-11 00:09:33 -0400

    Feature/scripts (#23)
    * New cards added.
    
    * Updating templates.
    
    * The English language pack for the script.
    
    * New scripts feature!
    
    * Order calls alphabetically.
    
    * Order cards alphabetically.
    
    * Addressing feedbacks.
    
    * Addressing feedbacks.
    
    * Added Spanish language pack for the script. (#27)
    
    * Addressing feedbacks
    
    * Addressing feedbacks.

---
+++ moderator.js
@@ -0,0 +1,19 @@
+var lang = require('./languages');
+var bindings = require('./bindings');
+var secuence = require('./secuence');
+
+exports.getScriptFromDeck = function (deck) {
+    var script = '';
+    var calls = new Set();
+    Object.keys(deck).forEach(function(role) {
+        if(bindings[role]) bindings[role].forEach(function (call) {
+            calls.add(call);
+        });
+    });
+    secuence.forEach(function (call){
+        if(calls.has(call)){
+            script += '- ' + lang.default[call] + '\n';
+        }
+    });
+    return script;
+}
