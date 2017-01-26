8481f0f3a7363b4ac2aa54db41c9425d3d6bbd88
Vetom <tomasverasm@gmail.com>
2017-01-26 19:31:04 -0400

    Adding cards format test.

--- tests/cards.js
+++ tests/cards.js
@@ -3,16 +3,25 @@ const should = require('should');
 const cards = require('../src/cards');
 const cardsData = require('../data/cards');
 
-describe('cards test', () => {
-    it('should return a card template', () => {
+describe('Cards tests', () => {
+    it('should return all the cards', () => {
         cards.getAll().should.be.equal(cardsData);
     });
 
-    it('should return an empty array if deck doesn\t exists', () => {
-        cards.inDeck('what_the_what').should.deepEqual([]);
+    it('should be in correct format', () => {
+        const allCards = cards.getAll();
+        allCards.forEach((card) => {
+            card.should.have.property('role').which.is.a.String();
+            card.should.have.property('value').which.is.a.Number();
+            card.should.have.property('amount').which.is.a.Number();
+        });
     });
 
-    it('should return an specific card', () => {
+    it('should return an empty array if deck doesn\'t exists', () => {
+        cards.inDeck('f4k3').should.deepEqual([]);
+    });
+
+    it('should contain all cards in deck', () => {
         const amateurDeck = [
             { role: 'Mayor', value: 2, amount: 1 },
             { role: 'Prince', value: 3, amount: 1 },
