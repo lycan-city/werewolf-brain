d6f4dac945bc75481e47992cd363bc6037f43b5d
Vetom <tomasverasm@gmail.com>
2017-03-11 00:30:46 -0400

    Updating game object structure.

--- src/languages.js
+++ src/languages.js
@@ -2,11 +2,16 @@ const languages = require('../data/translations');
 
 exports.get = (lang = 'en') => languages[lang] || {};
 exports.translateDeck = function translateDeck(deck, lang = 'en') {
-    const pack = languages[lang].roles;
-    const roles = Object.keys(deck);
-    const translatedDeck = {};
-    roles.forEach((role) => {
-        translatedDeck[pack[role]] = deck[role];
+    const pack = languages[lang];
+    const cards = Object.keys(deck);
+    const translatedDeck = [];
+    cards.forEach((card) => {
+        translatedDeck.push({
+            key: card,
+            amount: deck[card],
+            role: pack.roles[card],
+            description: pack.descriptions[card]
+        });
     });
     return translatedDeck;
 };
