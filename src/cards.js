28b17cebfa7465d85fd05031729addae3e71f4bb
Vetom <tomasverasm@gmail.com>
2017-03-23 23:02:53 -0400

    Updating cards to use decks new format (low coupling #win).

--- src/cards.js
+++ src/cards.js
@@ -4,6 +4,10 @@ const cards = require('../data/cards');
 exports.getAll = () => cards;
 
 exports.inDeck = (deckName) => {
-    const deck = decks.get(deckName) || [];
-    return cards.filter(c => deck.includes(c.key));
+    const deck = decks.get(deckName) || {};
+    return cards.filter(c => deck[c.key]).map(c => ({
+        key: c.key,
+        value: c.value,
+        amount: deck[c.key]
+    }));
 };
