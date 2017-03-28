38b1835654ede95902fd9a99f0eba0996b51669d
Vetom <tomasverasm@gmail.com>
2017-03-28 00:37:01 -0400

    Adding new function getGameFromCustomDeck.

--- src/cards.js
+++ src/cards.js
@@ -3,6 +3,12 @@ const cards = require('../data/cards');
 
 exports.getAll = () => cards;
 
+exports.inCustomDeck = deck => Object.keys(cards).filter(k => deck[k]).map(k => ({
+    key: k,
+    value: cards[k],
+    amount: deck[k]
+}));
+
 exports.inDeck = (deckName) => {
     const deck = decks.get(deckName) || {};
     return Object.keys(cards).filter(k => deck[k]).map(k => ({
