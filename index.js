a74df6e3a26a32d48299b3d95e7b06f6510a6bc1
Vetom <tomasverasm@gmail.com>
2017-03-29 00:41:24 -0400

    Updating index.

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
+const assert = require('assert');
 const cards = require('./src/cards');
 const decks = require('./src/decks');
 const moderator = require('./src/moderator');
 const game = require('./src/game');
 const languages = require('./src/languages');
 
-function getAllCards() {
-    return cards.getAll();
-}
-
-function getBalancedGame(players, language = 'en', chosenCards = cards.getAll()) {
-    return game.create(players, language, chosenCards, game.mode.NORMAL);
-}
-
-function getGameFromDeck(players, deckName, language = 'en') {
-    const chosenCards = cards.inDeck(deckName);
-    return game.create(players, language, chosenCards, game.mode.NORMAL);
-}
-
-function getChaosGame(players, language = 'en', chosenCards = cards.getAll()) {
-    return game.create(players, language, chosenCards, game.mode.CHAOS);
-}
-
-function getChaosGameFromDeck(players, deckName, language = 'en') {
-    const chosenCards = cards.inDeck(deckName);
-    return game.create(players, language, chosenCards, game.mode.CHAOS);
-}
-
-exports.getAllCards = getAllCards;
-exports.getAllDecks = decks.getAll;
-exports.getInDeck = decks.get;
+exports.getCards = cards.getAll;
+exports.getDecks = decks.getAll;
+exports.gameModes = game.mode;
+exports.getDeck = decks.get;
 exports.getScriptFromDeck = moderator.getScriptFromDeck;
-exports.getBalancedGame = getBalancedGame;
-exports.getGameFromDeck = getGameFromDeck;
-exports.getChaosGame = getChaosGame;
-exports.getChaosGameFromDeck = getChaosGameFromDeck;
 exports.getLanguages = languages.getLanguages;
 exports.translateDeck = languages.translateDeck;
+
+exports.getGame = (players, options) => {
+    const language = options.language || 'en';
+    const mode = options.mode || game.mode.NORMAL;
+    const deck = options.deck || decks.get.all;
+    let deckName = options.deckName || 'custom';
+    deckName = deckName.toLowerCase();
+
+    assert(players > 0, 'Players must be greater than 0.');
+    assert(!decks[deckName] && deckName !== 'custom', `Deck ${deckName} is not defined.`);
+
+    if (decks[deckName]) return game.create(players, language, cards.inDeck(deckName), mode);
+    return game.create(players, language, cards.inCustomDeck(deck), mode);
+};
